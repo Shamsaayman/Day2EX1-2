7276e78e2c9da60196d21ef6f93863865ed9efd6
Shamsaayman <91327556+Shamsaayman@users.noreply.github.com>
2024-02-12 23:05:12 +0300

    Add files via upload

---
+++ Day1/src/Main.java
@@ -0,0 +1,188 @@
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+                         // 1. BMI
+//         System.out.println("Enter Weight :");
+//         int weight = s.nextInt();
+//        System.out.println("Enter Height :");
+//        double height = s.nextDouble();
+// System.out.println("BMI = " + weight /(height * height));
+
+                         // 2. PERCENTAGE
+
+//        System.out.println("Enter Obtained Marks :");
+//        double obtMarks = s.nextDouble();
+//        System.out.println("Enter Total Marks :");
+//        double TotalMarks = s.nextDouble();
+//   System.out.println("Percentage = " + obtMarks+ "%");
+
+
+                       //3. CURRENCY
+
+//        System.out.println("Enter Amount in USD :");
+//        double usdamount = s.nextDouble();
+//        System.out.println("Enter Exchange Rate :");
+//        double rate = s.nextDouble();
+//        System.out.println("Amount in EUR = " + (usdamount*rate)) ;
+//
+
+
+                          // 4. STRING
+//        String str = "Hello, World!";
+//        StringBuilder strb = new StringBuilder();
+//        strb.append(str);
+//        System.out.println(" Length of the String is : " + strb.length() + " And Reversed String is : " + strb.reverse());
+
+
+
+                            // 5. SENTENCE STRING
+//        String str = "The quick brown fox jumps over the lazy dog";
+//        String str2 = str.substring(10,20);
+//        System.out.println(str2) ;
+
+
+                                // 6. SENTENCE KEYWORD
+//        String str = "The quick brown fox jumps over the lazy dog";
+//        String keyword = "jumps";
+//        if(str.contains(keyword)){
+//            System.out.println("Keyword " + "'" + keyword + "'"  + " is present in the sentence") ;
+//        }
+//        else {
+//            System.out.println("Keyword " + "'" + keyword + "'"  + " is NOT present in the sentence") ;
+//
+//        }
+
+
+                             // 7. SENTENCE REPLACE
+//        String str = "The quick brown fox jumps over the lazy dog";
+//        String str2 = str.replaceAll("fox" , "cat");
+//       System.out.println(str2) ;
+
+
+
+                             // 8. COMPARE STRINGS
+//        String str1 = "Hello";
+//        String str2 = "hello";
+//       if( str1.equalsIgnoreCase(str2)){
+//           System.out.println("Strings are equal");
+//       }
+
+
+/////////////////////////////////////////////////////////
+
+                         // 1. USER ROLE
+
+//String str1 = "admin";
+//String str2 = "superuser";
+//String str3 = "user";
+//String role = "admin";
+//
+//if (role.equalsIgnoreCase(str1)){
+//    System.out.println("Welcome Admin");
+//}
+//else if (role.equalsIgnoreCase(str2)){
+//    System.out.println("Welcome Superuser");
+//}
+//else {
+//    System.out.println("Welcome User");
+//}
+
+
+                      // 2.GREATEST NUMBER
+//        System.out.print("Enter First number: ");
+//        int num1 = s.nextInt();
+//        System.out.print("Enter Second number: ");
+//        int num2 = s.nextInt();
+//        System.out.print("Enter Third number: ");
+//        int num3 = s.nextInt();
+//
+//        if (num1 > num2 && num1 > num3) {
+//            System.out.println("The greatest: " + num1);
+//        }
+//
+//        else if (num2 > num1 && num2 > num3) {
+//            System.out.println("The greatest: " + num2);
+//        }
+//
+//        else if (num3 > num1 && num3 > num2) {
+//            System.out.println("The greatest: " + num3);
+//        }
+
+
+                        // 3. WEEKDAY
+//        System.out.println("Choose a Number from 1 to 7 :");
+//        String day = s.next();
+//
+//        switch(day){
+//            case "1":
+//                System.out.println("Sunday");
+//                break;
+//            case "2":
+//                System.out.println("Monday");
+//                break;
+//            case "3":
+//                System.out.println("Tuesday");
+//                break;
+//            case "4":
+//                System.out.println("Wednesday");
+//                break;
+//            case "5":
+//                System.out.println("Thursday");
+//                break;
+//            case "6":
+//                System.out.println("Friday");
+//                break;
+//            case "7":
+//                System.out.println("Saturday");
+//                break;
+//
+//        }
+
+                  //4. NUMERIC SCORE
+
+//        System.out.println("Enter your Numeric Score:");
+//        int score = s.nextInt();
+//
+//        if (score>89 && score<101){
+//            System.out.println("Letter Grade : A ");
+//        }
+//       else  if (score>79 && score<90){
+//            System.out.println("Letter Grade : B ");
+//        }
+//       else if (score>69 && score<80){
+//            System.out.println("Letter Grade : C ");
+//        }
+//       else if (score>59 && score<70){
+//            System.out.println("Letter Grade : D ");
+//        }
+//else if (score>0 && score<60){
+//    System.out.println("Letter Grade : F ");
+//}
+
+             // 5. AGE
+//        System.out.println("Enter your Age:");
+//        int age = s.nextInt();
+//
+//        if (age<13){
+//            System.out.println("You are a Child ");
+//        }
+//        else  if (age>12 && age<20){
+//            System.out.println("You are a Teenager ");
+//        }
+//        else if (age>19){
+//            System.out.println("You are an Adult ");
+//        }
+
+
+
+
+
+
+    }
+}
